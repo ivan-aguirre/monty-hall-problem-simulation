df73b4123bb180c51ffcb395747d6c57f8613525
iaguirre <iaguirre@motorola.com>
2020-06-23 16:29:45 -0300

    Fully functional version

---
+++ montyhall/simulation.py
@@ -0,0 +1,72 @@
+import argparse
+import random
+from itertools import starmap
+
+
+class Game:
+    CAR = 1
+    GOAT = 0
+
+    def __init__(self):
+        self.doors = None
+        self.winning_door = None
+        self.chosen_door = None
+        self.revealed_door = None
+
+    @staticmethod
+    def _present_doors():
+        return [Game.CAR, Game.GOAT, Game.GOAT]
+
+    def _chose_door(self):
+        valid_doors = list(range(0, len(self.doors)))
+        return random.choice(valid_doors)
+
+    def _possible_choices(self, enum_criteria):
+        return list(starmap(lambda door_position, door_content: door_position,
+                            filter(enum_criteria, enumerate(self.doors))))
+
+    def _reveal_door(self):
+        possible_choices = self._possible_choices(lambda t: t[0] != self.chosen_door and t[1] != Game.CAR)
+        return random.choice(possible_choices)
+
+    def change_door(self):
+        possible_doors_enum = self._possible_choices(lambda t: t[0] != self.chosen_door and t[0] != self.revealed_door)
+        self.chosen_door = random.choice(possible_doors_enum)
+
+    def play(self):
+        self.doors = Game._present_doors()
+        self.winning_door = self.doors.index(Game.CAR)
+        self.chosen_door = self._chose_door()
+        self.revealed_door = self._reveal_door()
+
+    def winner(self):
+        return self.chosen_door == self.winning_door
+
+
+def _simulate(n_simulations, change):
+    wins = 0
+    for _ in range(0, n_simulations):
+        game = Game()
+        game.play()
+        if change:
+            game.change_door()
+        if game.winner():
+            wins += 1
+    print(f"Simulations: {n_simulations}")
+    print(f"Wins.......: {wins} ({100 * wins / n_simulations}%)")
+    print(f"Losses.....: {n_simulations - wins} ({100 * (1 - (wins / n_simulations))}%)")
+
+
+def simulate(n_simulations):
+    print("Simulation without changing the chosen door")
+    _simulate(n_simulations, change=False)
+    print("")
+    print("Simulation changing the chosen door")
+    _simulate(n_simulations, change=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n_simulations", help="number of simulations", type=int)
+    args = parser.parse_args()
+    simulate(int(args.n_simulations))
--- tests/test.py
+++ tests/test.py
@@ -1,49 +1,59 @@
-import random
 import unittest
 
+from simulation import Game
 
 """
 - Monty Hall presents 3 doors. OK
+- One door must have a car, all others must have a goat.
 - Competitor chooses one door. OK
-- Monty Hall reveals one of the doors, other than the chosen door, that have a goat.
-- Competitor may keep or change the door.
-- S/he can't choose the revealed door (obvious).
-- If the door contains the goat the competitor looses, if there's a car the competitor wins.
-- N games can be played.
-- The simulation must keep the numbers of wins and loses.
-- At the end of simulation the stats must be displayed:
-    # Plays
-    # Win
-    # Loses
-  And the respective % 
+- Monty Hall reveals one of the doors. OK
+- The revealed door must not be the chosen door. OK
+- Also the revealed door must not have the car. OK
+- Competitor may keep or change the door. OK
+- S/he can't choose the revealed door (obvious). OK
+- If the door contains the goat the competitor looses, if there's a car the competitor wins. OK
 """
 
 
-def present_doors():
-    return [0, 0, 0]
-
-
-def choose_door(valid_doors):
-    return random.choice(valid_doors)
-
-def reveal_door():
-    return 1
-
-
-class TestStringMethods(unittest.TestCase):
+class TestSimulationMethods(unittest.TestCase):
 
     def test_present_doors(self):
-        doors = present_doors()
-        self.assertEquals(3, len(doors))
+        simulation = Game()
+        simulation.play()
+
+        doors = simulation.doors
+        self.assertEqual(3, len(doors))
+        self.assertEqual(1, doors.count(Game.CAR))
+        self.assertEqual(2, doors.count(Game.GOAT))
 
         valid_doors = list(range(0, len(doors)))
 
-        choice = choose_door(valid_doors)
-        self.assertIn(choice, valid_doors)
+        for _ in range(0, 10):
+            self.assertIn(simulation.chosen_door, valid_doors)
 
-        revealed = reveal_door()
-        self.assertIn(revealed, valid_doors)
-        self.assertNotEqual(choice, revealed)
+            revealed = simulation.revealed_door
+            self.assertIn(revealed, valid_doors)
+            self.assertNotEqual(simulation.chosen_door, revealed)
+            self.assertNotEqual(Game.CAR, doors[revealed])
+
+            for _ in range(0, 5):
+                past_choice = simulation.chosen_door
+                simulation.change_door()
+                new_choice = simulation.chosen_door
+                self.assertNotEqual(past_choice, simulation.chosen_door)
+                self.assertNotEqual(simulation.chosen_door, revealed)
+                self.assertIn(new_choice, valid_doors)
+
+    def test_victory_count(self):
+        simulation = Game()
+        simulation.play()
+        simulation.chosen_door = simulation.winning_door
+        self.assertEqual(True, simulation.winner())
+
+        simulation = Game()
+        simulation.play()
+        simulation.chosen_door = simulation.winning_door + 1
+        self.assertEqual(False, simulation.winner())
 
 
 if __name__ == '__main__':
